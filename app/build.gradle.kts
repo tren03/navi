b63796d757cb48b5a568e33bee71b3152b262936
vishnu <vishnusethuraman@gmail.com>
2025-01-16 15:47:02 +0530

    Added map plan for floor and scaled it down to show on phone

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,9 +37,12 @@ android {
 
 dependencies {
 
+    implementation(libs.zoomage)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/example/navi/Map.kt
@@ -0,0 +1,12 @@
+package com.example.navi
+
+import android.os.Bundle
+import com.jsibbold.zoomage.ZoomageView
+import androidx.appcompat.app.AppCompatActivity
+
+class Map : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+    }
+}
